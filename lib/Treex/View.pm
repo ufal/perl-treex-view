2e13aab601f8df108c07a152fcb7517e056c5e15
Michal Sedlák <sedlakmichal@gmail.com>
2015-11-10 19:47:53 +0100

    Make electron work

--- lib/Treex/View.pm
+++ lib/Treex/View.pm
@@ -10,12 +10,32 @@ use Treex::PML::Factory;
 use Treex::View::TreeLayout;
 use Treex::View::Node;
 use JSON;
+
+use File::Basename 'dirname';
+use File::Spec ();
+use File::ShareDir 'dist_dir';
+
 use namespace::autoclean;
 
 has 'tree_layout' => (
-    is => 'ro',
-    isa => 'Treex::View::TreeLayout',
-    default => sub { Treex::View::TreeLayout->new },
+  is => 'ro',
+  isa => 'Treex::View::TreeLayout',
+  default => sub { Treex::View::TreeLayout->new },
+);
+
+has 'static_dir' => (
+  is => 'ro',
+  default => sub {
+    my $shared_dir = eval { dist_dir(__PACKAGE__) };
+
+    # Assume installation
+    unless ($shared_dir) {
+      my $updir = File::Spec->updir();
+      $shared_dir = File::Spec->catdir(dirname(__FILE__), $updir, $updir, 'share');
+    }
+
+    File::Spec->catdir($shared_dir, 'static')
+  }
 );
 
 # fake TredMacro package so that TMT TrEd macros can be used directly
@@ -28,28 +48,27 @@ has 'tree_layout' => (
   use Exporter 'import';
   sub FS { $grp->{FSFile}->FS } # used by TectoMT_TredMacros.mak
   sub GetStyles {               # used by TectoMT_TredMacros.mak
-      my ($styles,$style,$feature)=@_;
-      my $s = $styles->{$style} || return;
-      if (defined $feature) {
-          return $s->{ $feature };
-      } else {
-          return %$s;
-      }
+    my ($styles,$style,$feature)=@_;
+    my $s = $styles->{$style} || return;
+    if (defined $feature) {
+      return $s->{ $feature };
+    } else {
+      return %$s;
+    }
   }
 
   sub AddStyle {
-      my ($styles,$style,%s)=@_;
-      if (exists($styles->{$style})) {
-          $styles->{$style}{$_}=$s{$_} for keys %s;
-      } else {
-          $styles->{$style}=\%s;
-      }
+    my ($styles,$style,%s)=@_;
+    if (exists($styles->{$style})) {
+      $styles->{$style}{$_}=$s{$_} for keys %s;
+    } else {
+      $styles->{$style}=\%s;
+    }
   }
 
   sub ListV {
-      UNIVERSAL::DOES::does($_[0], 'Treex::PML::List') ? @{$_[0]} : ()
+    UNIVERSAL::DOES::does($_[0], 'Treex::PML::List') ? @{$_[0]} : ()
   }
-
 
   # maybe more will be needed for drawing arrows, need example
 }
@@ -62,34 +81,34 @@ sub convert {
 
   my @bundles;
   foreach my $bundle ($doc->get_bundles) {
-      my %bundle;
-      my %zones;
-      $bundle{zones} = \%zones;
-      foreach my $zone ( $bundle->get_all_zones ) {
-          my %trees;
-          foreach my $tree ( $zone->get_all_trees ) {
-              my $tree_label = $self->tree_layout->get_tree_label($tree);
-              my @nodes = (map { Treex::View::Node->new(node => $_, labels => $labels) }
-                               $self->tree_layout->get_nodes($tree));
-              $trees{$tree_label} = {
-                  nodes => \@nodes,
-                  language => $tree->language,
-                  layer => $tree->get_layer,
-              };
-          }
-          $zones{$self->tree_layout->get_zone_label($zone)} = {
-              trees => \%trees,
-              sentence => $zone->sentence
-          };
+    my %bundle;
+    my %zones;
+    $bundle{zones} = \%zones;
+    foreach my $zone ( $bundle->get_all_zones ) {
+      my %trees;
+      foreach my $tree ( $zone->get_all_trees ) {
+        my $tree_label = $self->tree_layout->get_tree_label($tree);
+        my @nodes = (map { Treex::View::Node->new(node => $_, labels => $labels) }
+                         $self->tree_layout->get_nodes($tree));
+        $trees{$tree_label} = {
+          nodes => \@nodes,
+          language => $tree->language,
+          layer => $tree->get_layer,
+        };
       }
-      $bundle{desc} = $self->tree_layout->value_line($bundle);
-      push @bundles, \%bundle;
+      $zones{$self->tree_layout->get_zone_label($zone)} = {
+        trees => \%trees,
+        sentence => $zone->sentence
+      };
+    }
+    $bundle{desc} = $self->tree_layout->value_line($bundle);
+    push @bundles, \%bundle;
   }
 
   my $json = JSON->new->allow_nonref->allow_blessed->convert_blessed;
   $json = $json->pretty if $pretty;
 
-  return $json->encode({ print => \@bundles});
+  return $json->encode(\@bundles);
 }
 
 __PACKAGE__->meta->make_immutable;
